afb3151a84a643f97c3d1018905916731c246d58
Mendel <mkonikov123@gmail.com>
2021-04-30 14:43:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, surprise){ 
+
+    const newArray = [];
+    for (let x = 0; x < names.length; x++){
+        console.log(newArray.push(`Thank you, ${names[x]}, for the wonderful ${surprise} gift!`));
+      }
+      return newArray;
+  }
+
+  function countDown(a){
+    while (a >= 0) {
+      console.log(a--);
+    } 
+}
+  
+
+
+
+
+
+
+
+
+
